1e39489e48d73aa12db15d733dd8f307ac0f1d69
b08901152 <b08901152@ntu.edu.tw>
2021-06-28 22:35:42 +0800

    add game.py

--- socket/network.py
+++ socket/network.py
@@ -1,16 +1,17 @@
 import socket
+import pickle
 
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "YOUR LOCAL IPV4"
+        self.server = "192.168.1.101"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
+        self.p = self.connect()
 
-    def getPos(self):
-        return self.pos
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
@@ -22,6 +23,7 @@ class Network:
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048*2))
         except socket.error as e:
             print(e)
+
