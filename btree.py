1ec12ee180941826a0202c9c2ed490420c2e4a2f
melody16843 <75027048+melody16843@users.noreply.github.com>
2021-06-30 01:09:13 +0800

    update avl binary tree

--- btree.py
+++ btree.py
@@ -3,23 +3,22 @@ class tree():
     def __init__(self):
         self.root=None
 
-    def insert(self,data):
+    def insert(self,node):
         end=False
-        node=user(data[0],data[1],data[2],data[3])
         if self.root==None:
             self.root=node
             end=True
         present=self.root
         while end==False:
-            if node.id>present.value and present.right_child==None:
+            if node.id>present.id and present.right_child==None:
                 present.right_child=node
                 end=True
-            elif node.id>present.value and present.right_child!=None:
+            elif node.id>present.id and present.right_child!=None:
                 present=present.right_child
-            elif node.id<present.value and present.left_child==None:
+            elif node.id<present.id and present.left_child==None:
                 present.left_child=node
                 end=True
-            elif node.id<present.value and present.left_child!=None:
+            elif node.id<present.id and present.left_child!=None:
                 present=present.left_child
     def search(self,index):
         current=self.root
@@ -32,55 +31,34 @@ class tree():
     def change(self,node1,node2):
         node1.id=node2.id
         node1.name=node2.name
-        node1.hobby=node2.hobby
+        node1.interest=node2.interest
         node1.gender=node2.gender
         node1.age=node2.age
-    def delete(self,index):
-        end=False
-        present=self.root
-        parent=None
-        while end==False: #search
-            if index>present.id and present.right_child==None:
-                break
-            elif index>present.id and present.right_child!=None:
-                parent=present
-                present=present.right_child
-            elif index<present.id and present.left_child==None:
-                break
-            elif index<present.id and present.left_child!=None:
-                parent=present
-                present=present.left_child
-            elif index==present.id:
-                end=True
-        if end==True: #do delete
-            if present.left_child!=None and present.right_child!= None: #for delete node has two children
-                right_min=present.right_child
-                while right_min.left_child:
-                    right_min=right_min.left_child
-                if present==self.root:
-                    self.change(self.root,right_min)
-                    self.delete(right_min.id)
-                else:
-                    self.change(parent,right_min)
-                    self.delete(right_min.id)
-            elif present.left_child==None and present.right_child==None: #for delete node has no child
-                if parent.right_child==present:
-                    parent.right_child=None
-                else:
-                    parent.left_child=None
-            else: #for delete node has one child
-                if present==self.root:
-                    if present.left_child!=None:
-                        self.root=present.left_child
-                    else:
-                        self.root=present.right_child
-                elif present.left_child!=None:
-                    if parent.left_child==present:
-                        parent.left_child=present.left_child
-                    else:
-                        parent.right_child=present.left_child
-                elif present.right_child!=None:
-                    if parent.left_child==present:
-                        parent.left_child=present.right_child
-                    else:
-                        parent.right_child=present.right_child
+    def delete(self, root, user):
+    
+        # Find the node to be deleted and remove it
+        if not root:
+            return root
+        elif user.id < root.id:
+            root.left_child = self.delete_node(root.left_child, user.id)
+        elif user.id > root.id:
+            root.right_child = self.delete_node(root.right_child, user.id)
+        else:
+            if root.left_child is None:
+                temp = root.right_child
+                root = None
+                return temp
+            elif root.right_child is None:
+                temp = root.left_child
+                root = None
+                return temp
+            temp = self.getMinValueNode(root.right_child)
+            root.id = temp.id
+            root.right_child = self.delete(root.right_child,
+                                          temp.id)
+        if root is None:
+            return root
+    def getMinValueNode(self, root):
+        if root is None or root.left_child is None:
+            return root
+        return self.getMinValueNode(root.left_child)
