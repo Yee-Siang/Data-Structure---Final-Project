cf2092836772955579f7b90215f1b453d68d9b40
鐘郁翔 <55400183+Yee-Siang@users.noreply.github.com>
2021-07-03 12:08:50 +0800

    Update user_and_pair.py

--- user_and_pair.py
+++ user_and_pair.py
@@ -1,9 +1,14 @@
+import pandas as pd
+import numpy as np
+import json
 from hungarian import *
 import random
 ls = list(range(1000000))
 random.shuffle(ls)
+
+
 class user():
-    def __init__(self, name, age, gender, interest, ideal_age, ideal_gender, identity = None):
+    def __init__(self, name, age, gender, interest, ideal_age, ideal_gender, identity=None):
         if identity == None:
             self.id = ls[0]
             del ls[0]
@@ -16,14 +21,16 @@ class user():
         self.left_child = None
         self.right_child = None
         self.paired = []
+        self.paired_id = []
+
     def difference(self, other):
-        
+
         if self == other:
             return 100000
-        
+
         if other in self.paired:
             return 100000
-        
+
         val = 0
         if self.ideal_gender != other.gender:
             val += 25
@@ -36,9 +43,6 @@ class user():
         val += 2*abs(self.ideal_age - other.age)
         return val
 
-import pandas as pd
-import json
-import numpy as np
 
 class NpEncoder(json.JSONEncoder):
     def default(self, obj):
@@ -50,18 +54,25 @@ class NpEncoder(json.JSONEncoder):
             return obj.tolist()
         else:
             return super(NpEncoder, self).default(obj)
-        
+
+
+#############這是測試資料使用的數據############################
 df = pd.read_excel('data.xlsx')
 rows = df.shape[0]
 user_ls = []
 user_ls_out = []
 for j in range(rows):
-    individual = user(df.iat[j,3], int(df.iat[j,2]), df.iat[j,0], df.iat[j,1].split(' '), df.iat[j,4], df.iat[j,5])
+    individual = user(df.iat[j, 3], int(
+        df.iat[j, 2]), df.iat[j, 0], df.iat[j, 1].split(' '), df.iat[j, 4], df.iat[j, 5])
     print(individual.name, individual.id)
     user_ls.append(individual)
-    user_ls_out.append({'id':individual.id, 'name':individual.name, 'age':individual.age, 'gender':individual.gender, 'interest':individual.interest, 'ideal_age':individual.ideal_age, 'ideal_gender':individual.ideal_gender, 'paired':individual.paired})
+    user_ls_out.append({'id': individual.id, 'name': individual.name, 'age': individual.age, 'gender': individual.gender,
+                        'interest': individual.interest, 'ideal_age': individual.ideal_age, 'ideal_gender': individual.ideal_gender, 'paired_id': individual.paired_id})
 
-def pairing_process(user_ls, file2, file_user):        
+########################################################################
+
+
+def pairing_process(user_ls, file2, file_user):
     cost = []
     for k in range(len(user_ls)):
         each_cost = []
@@ -76,16 +87,17 @@ def pairing_process(user_ls, file2, file_user):
     hungarian = Hungarian(cost)
     print('calculating...')
     hungarian.calculate()
-    print("Calculated value:\t", hungarian.get_total_potential())  
+    print("Calculated value:\t", hungarian.get_total_potential())
     pairs = hungarian.get_results()
     print("Results:\n\t", pairs)
     print(len(pairs))
     print("-" * 80)
-    with open(file2, 'w', newline = '') as jsonfile:
-        json.dump(pairs, jsonfile, cls = NpEncoder)
+    with open(file2, 'w', newline='') as jsonfile:
+        json.dump(pairs, jsonfile, cls=NpEncoder)
     for i in range(len(pairs)):
         user_ls[pairs[i][0]].paired.append(user_ls[pairs[i][1]])
-    #with open(file_user, 'w', newline = '') as user_json:
+        user_ls[pairs[i][0]].paired_id.append(user_ls[pairs[i][1].id])
+    # with open(file_user, 'w', newline = '') as user_json:
      #   json.dump(user_ls_out, user_json, cls = NpEncoder)
 
 #pairing_process(user_ls,'pairing.json', 'user_data.json')
