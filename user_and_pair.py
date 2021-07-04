7dffda1ac20d7b9eba5b37f48ac5a5ee77e7a5df
鐘郁翔 <55400183+Yee-Siang@users.noreply.github.com>
2021-07-04 12:37:40 +0800

    Update user_and_pair.py
    seperate

--- user_and_pair.py
+++ user_and_pair.py
@@ -57,6 +57,7 @@ class NpEncoder(json.JSONEncoder):
 
 
 #############這是測試資料使用的數據############################
+'''
 df = pd.read_excel('data.xlsx')
 rows = df.shape[0]
 user_ls = []
@@ -68,36 +69,8 @@ for j in range(rows):
     user_ls.append(individual)
     user_ls_out.append({'id': individual.id, 'name': individual.name, 'age': individual.age, 'gender': individual.gender,
                         'interest': individual.interest, 'ideal_age': individual.ideal_age, 'ideal_gender': individual.ideal_gender, 'paired_id': individual.paired_id})
-
+    
+'''
 ########################################################################
-
-
-def pairing_process(user_ls, file2, file_user):
-    cost = []
-    for k in range(len(user_ls)):
-        each_cost = []
-        for m in range(len(user_ls)):
-            each_cost.append(user_ls[k].difference(user_ls[m]))
-        cost.append(each_cost)
-    print(cost)
-    print(len(cost))
-    print(len(cost[0]))
-    print(len(cost[1]))
-  #  print(user_ls_out)
-    hungarian = Hungarian(cost)
-    print('calculating...')
-    hungarian.calculate()
-    print("Calculated value:\t", hungarian.get_total_potential())
-    pairs = hungarian.get_results()
-    print("Results:\n\t", pairs)
-    print(len(pairs))
-    print("-" * 80)
-    with open(file2, 'w', newline='') as jsonfile:
-        json.dump(pairs, jsonfile, cls=NpEncoder)
-    for i in range(len(pairs)):
-        user_ls[pairs[i][0]].paired.append(user_ls[pairs[i][1]])
-        user_ls[pairs[i][0]].paired_id.append(user_ls[pairs[i][1].id])
-    # with open(file_user, 'w', newline = '') as user_json:
-     #   json.dump(user_ls_out, user_json, cls = NpEncoder)
 
 #pairing_process(user_ls,'pairing.json', 'user_data.json')
