ad7852e8ca3a5fd9b482149bd6814473342d6089
鐘郁翔 <55400183+Yee-Siang@users.noreply.github.com>
2021-07-05 17:37:03 +0800

    Update pairing.py

--- server/pairing.py
+++ server/pairing.py
@@ -14,9 +14,11 @@ def pairing_process(user_ls, file2, file_user):
     '''
     with open(file2, 'w', newline='') as jsonfile:
         json.dump(pairs, jsonfile, cls = NpEncoder)
+    '''
     for i in range(len(pairs)):
         user_ls[pairs[i][0]].paired.append(user_ls[pairs[i][1]])
         user_ls[pairs[i][0]].paired_id.append(user_ls[pairs[i][1].id])
+    '''
     with open(file_user, 'w', newline = '') as user_json:
         json.dump(user_ls_out, user_json, cls = NpEncoder)
     '''
